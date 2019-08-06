4e5317300aae641792a003ce10724701dea6fb22
Tom H Anderson <tom.h.anderson@gmail.com>
2019-08-06 11:39:19 -0600

    removed @app alias; kept @env alias

--- src/app/data/service/json-api.service.ts
+++ src/app/data/service/json-api.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { delay, catchError } from 'rxjs/operators';
-
-import { environment } from '../../../environments/environment';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
