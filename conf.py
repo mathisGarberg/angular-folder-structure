374c18b47f85c2fd8794d4664b8d642185e98ada
Tom H Anderson <tom.h.anderson@gmail.com>
2019-08-10 12:29:36 -0600

    Added Tom H Anderson as author

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 project = 'Angular Folder Structure'
 author = 'Mathis Garberg, Tom H Anderson <tom.h.anderson@gmail.com>'
-copyright = '2019 Mathis Garberg and contributors'
+copyright = '2019 Mathis Garberg, Tom H Anderson <tom.h.anderson@gmail.com>, and contributors'
 master_doc = 'index'
 show_sphinx = False
 commit = False
