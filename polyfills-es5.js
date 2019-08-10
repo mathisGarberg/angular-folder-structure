9d45f732f5871b266af4d3bf1d89951b4f04efe9
Tom H Anderson <tom.h.anderson@gmail.com>
2019-08-10 06:09:09 -0600

    gh-pages master@af1df7d (gh-pages branch) (#50)
    * Cleared out old version
    
    * New version master@af1df7d

--- polyfills-es5.js
+++ polyfills-es5.js
@@ -8617,8 +8617,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure2/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
-module.exports = __webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure2/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
+__webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
+module.exports = __webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
 
 
 /***/ })
--- polyfills.js
+++ polyfills.js
@@ -11949,8 +11949,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure2/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure2/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/tom.h.anderson/Projects/apiskeletons/angular-folder-structure/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
