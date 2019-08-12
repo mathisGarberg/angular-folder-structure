f17c99f856344057edb2a7467669871253b7c7f6
Tom H Anderson <tom.h.anderson@gmail.com>
2019-08-11 20:51:32 -0600

    Refactored layout

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,28 +1,12 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { environment as env } from '@env/environment';
-import { Observable } from 'rxjs/Observable';
 import { ThemeService } from '../../core/service/theme.service';
+import { Subject, Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-  @Output() changeTheme: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-  year = new Date().getFullYear();
-  version = 2;
-  envName = env.envName;
-  isDarkTheme: Observable<boolean>;
-
-    constructor(private themeService: ThemeService) { }
-
-  ngOnInit(): void {
-    this.isDarkTheme = this.themeService.isDarkTheme;
-  }
-
-  toggleDarkTheme(checked: boolean) {
-    this.changeTheme.emit(checked);
-  }
+export class FooterComponent {
 }
--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
 import { environment } from '@env/environment';
+import { Observable } from 'rxjs';
+import { ThemeService } from 'app/core/service/theme.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
-  version = environment.version;
-  repoUrl = 'https://github.com/mathisGarberg/angular-folder-structure';
+export class NavComponent implements OnInit {
+  public version = environment.version;
+  public repoUrl = 'https://github.com/mathisGarberg/angular-folder-structure';
+  public isDarkTheme$: Observable<boolean>;
 
   navItems = [
     { link: '/dashboard/home', title: 'Home' },
     { link: '/about', title: 'About' },
     { link: '/contact', title: 'Contact' }
   ];
+
+  constructor(
+    private themeService: ThemeService
+  ) {}
+
+  ngOnInit() {
+    this.isDarkTheme$ = this.themeService.getDarkTheme();
+  }
+
+  toggleTheme(checked: boolean) {
+    this.themeService.setDarkTheme(checked);
+  }
 }
