3dbcbb21ed75e2672784dc468b314d20b1aa6060
Mathis <mathisGarberg@users.noreply.github.com>
2021-02-13 18:41:57 +0100

    ISSUE-114: Change from tslint to eslint (#117)

--- src/app/modules/contact/page/contact/contact.component.spec.ts
+++ src/app/modules/contact/page/contact/contact.component.spec.ts
@@ -1,4 +1,4 @@
-/* tslint:disable:no-unused-variable */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
