964b3171b978f5bea3886f890fc0104d63d80773
Mathis Garberg <Mathis.Garberg@gmail.com>
2022-07-25 10:41:19 +0200

    150: Removed modal

--- src/app/modules/home/page/home.component.ts
+++ src/app/modules/home/page/home.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { ProjectService } from '@data/service/project.service';
 import { Project } from '@data/schema/project';
-import { MyModalComponent } from '../modal/my-modal.component';
 
 @Component({
   selector: 'app-home',
@@ -14,21 +12,5 @@ import { MyModalComponent } from '../modal/my-modal.component';
 export class HomeComponent {
   projects$: Observable<Project[]> = this.projectService.getAll();
 
-  constructor(
-    private modalService: NgbModal,
-    private projectService: ProjectService
-  ) {}
-
-  openMyModal() {
-    const modalRef = this.modalService.open(MyModalComponent);
-    modalRef.componentInstance.id = 1;
-    modalRef.result.then(
-      result => {
-        console.log(result);
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
+  constructor(private projectService: ProjectService) {}
 }
